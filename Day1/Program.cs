5ddbaf266b7c4a2275a0d6d866cf3f9e93494734
ZKH\BosmanARG <a.r.g.bosman@umcg.nl>
2022-12-01 22:20:27 +0100

    Add project files.

---
+++ Day1/Program.cs
@@ -0,0 +1,34 @@
+﻿var input = File.ReadAllLines(@"Input.txt");
+var food = new List<List<int>>();
+var calories = new List<int>();
+foreach (var line in input)
+{
+    if (!string.IsNullOrEmpty(line)) {
+        calories.Add(int.Parse(line));
+    } else
+    {
+        food.Add(calories);
+        calories = new List<int>();
+    }
+}
+if (calories.Any())
+{
+    food.Add(calories);
+}
+
+
+Part1();
+Part2();
+
+void Part1()
+{
+    var maxCalories = food.Select(item => item.Sum()).Max();
+    Console.WriteLine($"Max calories carried by an elf: {maxCalories}"); // 69281
+}
+
+void Part2()
+{
+    var totalCalories = food.Select(item => item.Sum()).
+        OrderByDescending(i=>i).Take(3).Sum();
+    Console.WriteLine($"Total calories carried by top 3 elves: {totalCalories}"); // 201524
+}
